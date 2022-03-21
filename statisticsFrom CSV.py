aa538aea07c4e11c25458d601325608f237337f5
Joao <jrodriiguespro@gmail.com>
2022-03-21 12:13:03 +0000

    .csv files and filereader

---
+++ statisticsFrom CSV.py
@@ -0,0 +1,20 @@
+import csv
+from statisticsMU123 import Dataset
+
+filename = "data.csv"
+
+
+with open(filename) as f:
+
+    file_reader = csv.reader(f)
+    data_str = next(file_reader)   
+    f.close()
+
+data_int = []
+
+for item in data_str:
+    data_int.append(int(item))
+
+dataset = Dataset(data_int)
+print(f"Dataset details: {dataset}")
+
--- statisticsFromTXT.py
+++ statisticsFromTXT.py
@@ -1,25 +1,40 @@
-from matplotlib.pyplot import close
 from statisticsMU123 import Dataset
 
+# Filenames
 possible_filename = "possible.txt"
 probable_filename = "probable.txt"
+practiceQuiz_filename = "dataPracticeQuiz.txt"
+
+# Data storages
 possible_data = []
 probable_data = []
+practiceQuiz_data = []
 
 with open(possible_filename) as f:
     
     for line in f:
         possible_data.append(int(line.strip()))
-    close
+    f.close()
 
 with open(probable_filename) as f:
 
     for line in f:
         probable_data.append(int(line.strip()))
-    close
+    f.close()
 
+
+with open(practiceQuiz_filename) as f:
+    
+    for line in f:
+        practiceQuiz_data.append(int(line.strip()))
+    f.close()
+
+"""
 possible = Dataset(possible_data)
 probable = Dataset(probable_data)
+practiceQuiz = Dataset(practiceQuiz_data)
 print(f"Possible dataset:\n{possible}")
 print(f"\nProbable dataset:\n{probable}")
+print(f"\nPractice quiz dataset:\n{practiceQuiz}")
+"""
 
